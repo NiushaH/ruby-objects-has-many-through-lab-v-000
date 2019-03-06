1f8b1b78341b396d67d99025ad8e0d72304cdf14
Niusha H <niusha.hadziomerovic@gmail.com>
2019-03-06 06:50:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,7 +15,7 @@ class Patient
   
   # takes in an argument of a doctor and a date and creates a new Appointment. The Appointment should know that it belongs to the patient
   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+    new_appointment = Appointment.new(self, doctor, date)
   end
 
   # iterates through the Appointments array and returns Appointments that belong to the patient
